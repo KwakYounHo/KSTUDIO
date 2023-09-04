8184cc679966546d9eabce714ab937dd3eb80d95
곽윤호 <kwakdm95@gmail.com>
2023-09-04 07:08:11 +0630

    modify: html language, add: body's className modift: html lang - en -> ko add: insert styleClass from tailwind CSS

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="ko">
+      <body className={`${inter.className} flex flex-col items-center h-screen`}>
         <Header />
         {children}
         <Footer />
