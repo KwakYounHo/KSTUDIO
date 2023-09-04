70d145d1840defec32f60984cfb7c2b408bda018
곽윤호 <kwakdm95@gmail.com>
2023-09-04 21:04:55 +0630

    add: add Blog page

---
+++ client/src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react'
+
+export const Blog: FC = () => {
+  return (
+    <main className={'my-auto'}>
+      <h1 className={'text-4xl'}>공사중</h1>
+    </main>
+  )
+}
+
+export default Blog
