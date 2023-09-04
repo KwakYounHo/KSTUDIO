9faf1956a30b41ea6eafa7144e02f363b104f4f7
곽윤호 <kwakdm95@gmail.com>
2023-09-04 21:01:08 +0630

    add: about page content, add: about description

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -1,30 +1,56 @@
 import { FC } from "react";
 import { Bad_Script } from "next/font/google";
 import type { NextFont } from "next/dist/compiled/@next/font";
-import { about } from '@/models/about/about'
+import { about } from "@/models/about/about";
+import Link from "next/link";
 
 const badScript: NextFont = Bad_Script({
-  subsets: ['latin'],
-  weight: ['400']
-})
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const About: FC = () => {
   return (
     <>
-      <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-      </head>
-      <main className={'flex flex-col items-center'}>
-        <div className={'w-52 h-52 my-8'}>
-          <img src={about.profile.profileIMG} title="avatar" className={'rouned drop-shadow-lg select-none'} />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+      />
+      <main className={"flex flex-col items-center"}>
+        <div className={"w-52 h-52 my-8"}>
+          <img
+            src={about.profile.profileIMG}
+            title="avatar"
+            className={"rouned drop-shadow-lg select-none"}
+          />
         </div>
-        <div className={'select-none'}>
-          <span className={`${badScript.className} text-3xl`}>{about.profile.name}</span>
-          <span className={'opacity-40'}>({about.profile.subName})</span>
+        <div className={"select-none"}>
+          <span className={`${badScript.className} text-3xl`}>
+            {about.profile.name}
+          </span>
+          <span className={"opacity-40"}>({about.profile.subName})</span>
+        </div>
+        <div className={"mt-11 flex flex-col"}>
+          {about.description.map((element) => {
+            return (
+              <div key={element.id} className={"flex items-center"}>
+                <span className={"material-symbols-outlined mr-1"}>
+                  {element.icon}
+                </span>
+                {element.href ? (
+                  <span className={"text-xs text-blue-400"}>
+                    <Link href={element.href} target="_blank">{element.text}</Link>
+                  </span>
+                ) : (
+                  <span className={"text-xs"}>{element.text}</span>
+                )}
+              </div>
+            );
+          })}
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
 export default About;
--- client/src/models/about/about.ts
+++ client/src/models/about/about.ts
@@ -3,5 +3,31 @@ export const about = {
     profileIMG: 'https://s.gravatar.com/avatar/23c28fd74ef3561338a391f7492d40a4?s=400',
     name: 'YounHo Kwak',
     subName: 'VedK'
-  }
+  },
+  description: [
+    {
+      id: 'job',
+      icon: 'taunt',
+      text: 'Developer',
+      href: ''
+    },
+    {
+      id: 'location',
+      icon: 'location_on',
+      text: 'Yangon Myanmar / Daejeon Korea',
+      href: ''
+    },
+    {
+      id: 'github',
+      icon: 'code',
+      text: 'github.com/KwakYounHo',
+      href: 'https://github.com/KwakYounHo'
+    },
+    {
+      id: 'naverBlog',
+      icon: 'Mode_Comment',
+      text: 'VedK - Naver Blog',
+      href: 'https://blog.naver.com/kwakdm_'
+    }
+  ]
 }
