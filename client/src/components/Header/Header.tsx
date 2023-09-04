94d1a2890425c030067a1864771e390e7297ea20
곽윤호 <kwakdm95@gmail.com>
2023-09-04 07:10:00 +0630

    modify: header title, add: add category to nav

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,29 @@
 import { FC } from "react"
+import { NextFont } from "next/dist/compiled/@next/font";
+import { Inconsolata } from "next/font/google";
+import Link from "next/link";
+import { title } from "@/models/header/navigation";
+
+const inconsolata: NextFont = Inconsolata({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900']
+})
+
 
 const Header: FC = () => {
   return (
-    <h1>헤더가 들어갈 곳</h1>
+    <header id="container" className={`${inconsolata.className} select-none`}>
+      <Link href='/' className='flex justify-center'>
+        <button className='hover:text-cyan-400'>
+          <h1 className='uppercase tracking-widest mt-8 mb-3'>kwakyounho</h1>
+        </button>
+      </Link>
+      <nav className='text-sm font-light text-gray-900 flex gap-4 justify-center capitalize tracking-tight m-5'>
+        {title.map(element=>{
+          return <Link href={element.url} className='hover:text-cyan-400'>{element.title}</Link>
+        })}
+      </nav>
+    </header>
   )
 }
 
---
+++ client/src/models/header/navigation.ts
@@ -0,0 +1,14 @@
+type Props = {
+  url: string
+  title: string
+}
+export const title: Props[] = [
+  {
+    url: '/about',
+    title: 'about'
+  },
+  {
+    url: '/blog',
+    title: 'blog'
+  }
+]
