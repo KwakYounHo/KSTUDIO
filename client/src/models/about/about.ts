59a1785288b9f46ac37939a4685373bc9d64ccf0
곽윤호 <kwakdm95@gmail.com>
2023-09-04 09:25:14 +0630

    fix: data architecture

--- client/src/models/about/about.ts
+++ client/src/models/about/about.ts
@@ -1,5 +1,7 @@
 export const about = {
-  profile: 'https://s.gravatar.com/avatar/23c28fd74ef3561338a391f7492d40a4?s=400',
-  name: 'YounHo Kwak',
-  subName: 'VedK'
+  profile: {
+    profileIMG: 'https://s.gravatar.com/avatar/23c28fd74ef3561338a391f7492d40a4?s=400',
+    name: 'YounHo Kwak',
+    subName: 'VedK'
+  }
 }
