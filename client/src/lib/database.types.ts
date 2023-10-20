1d706cd8dae68b44bd0cdb32afec3c3c46592c2d
곽윤호 <kwakdm95@gmail.com>
2023-10-20 13:59:52 +0630

    add: add quote table at database

--- client/src/lib/database.types.ts
+++ client/src/lib/database.types.ts
@@ -1,34 +1,71 @@
-import type { Article } from "@/utils/Article"
-
 export type Json =
   | string
   | number
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       posts: {
-        Row: Article
-        Insert: Article
-        Update: Article
-        Relationships: []
-      }
-    }
+        Row: {
+          article: string;
+          created_at: string;
+          seq: number;
+          slug: string;
+          title: string;
+          updated_at: string | null;
+        };
+        Insert: {
+          article: string;
+          created_at?: string;
+          seq?: number;
+          slug: string;
+          title: string;
+          updated_at?: string | null;
+        };
+        Update: {
+          article?: string;
+          created_at?: string;
+          seq?: number;
+          slug?: string;
+          title?: string;
+          updated_at?: string | null;
+        };
+        Relationships: [];
+      };
+      quote: {
+        Row: {
+          credit: string;
+          id: number;
+          quote: string;
+        };
+        Insert: {
+          credit: string;
+          id?: number;
+          quote: string;
+        };
+        Update: {
+          credit?: string;
+          id?: number;
+          quote?: string;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
