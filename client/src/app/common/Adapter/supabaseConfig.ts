dd14e305ec5e9ce6afcd7ca653fb4b0a9ae45401
곽윤호 <kwakdm95@gmail.com>
2023-10-24 12:12:26 +0630

    modify: modify env name

--- client/src/app/common/Adapter/supabaseConfig.ts
+++ client/src/app/common/Adapter/supabaseConfig.ts
@@ -2,5 +2,5 @@ import { ENV } from "@/app/common/env";
 
 export const supabaseConfig = {
   url: ENV.NEXT_PUBLIC_SUPABASE_URL,
-  key: ENV.NEXT_PUBLIC_SUPABASE_API_KEY,
+  key: ENV.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 };
--- client/src/app/common/env.ts
+++ client/src/app/common/env.ts
@@ -3,10 +3,10 @@ import { cleanEnv, str } from "envalid";
 export const ENV = cleanEnv(
   {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_API_KEY: process.env.NEXT_PUBLIC_SUPABASE_API_KEY,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
   {
     NEXT_PUBLIC_SUPABASE_URL: str(),
-    NEXT_PUBLIC_SUPABASE_API_KEY: str(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: str(),
   }
 );
