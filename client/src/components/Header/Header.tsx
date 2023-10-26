8f7672b45dc804f656e70a99211ea10a08ce5a8a
곽윤호 <kwakdm95@gmail.com>
2023-10-26 17:35:29 +0630

    apply: apply prettier

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,30 +1,38 @@
-import { FC } from "react"
+import { FC, useContext } from "react";
 import { Inconsolata } from "next/font/google";
 import Link from "next/link";
 import { title } from "@/models/header/navigation";
+
 import type { NextFont } from "next/dist/compiled/@next/font";
 
 const inconsolata: NextFont = Inconsolata({
-  subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700', '800', '900']
-})
-
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const Header: FC = () => {
   return (
     <header id="container" className={`${inconsolata.className} select-none`}>
-      <Link href='/' className='flex justify-center'>
-        <button className='hover:text-blue-400'>
-          <h1 className='uppercase tracking-[3px] mt-12 mb-3'>younho kwak</h1>
+      <Link href="/" className="flex justify-center">
+        <button className="hover:text-blue-400">
+          <h1 className="uppercase tracking-[3px] mt-12 mb-3">younho kwak</h1>
         </button>
       </Link>
-      <nav className='text-sm font-light text-gray-700 flex gap-4 justify-center capitalize tracking-tight m-4'>
-        {title.map(element=>{
-          return <Link href={element.url} className='hover:text-blue-400' key={element.id}>{element.title}</Link>
+      <nav className="text-sm font-light text-gray-700 flex gap-4 justify-center capitalize tracking-tight m-4">
+        {title.map((element) => {
+          return (
+            <Link
+              href={element.url}
+              className="hover:text-blue-400"
+              key={element.id}
+            >
+              {element.title}
+            </Link>
+          );
         })}
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
