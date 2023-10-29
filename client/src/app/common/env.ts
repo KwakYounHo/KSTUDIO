2688e9bfa50532043e4cfc93daf43ce81856d33f
곽윤호 <kwakdm95@gmail.com>
2023-10-29 16:17:10 +0630

    add: add public schema manager ENV

--- client/src/app/common/env.ts
+++ client/src/app/common/env.ts
@@ -4,9 +4,11 @@ export const ENV = cleanEnv(
   {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    MANAGER_ID: process.env.MANAGER_ID,
   },
   {
     NEXT_PUBLIC_SUPABASE_URL: str(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: str(),
+    MANAGER_ID: str(),
   }
 );
