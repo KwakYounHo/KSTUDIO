b644d7b2a6d04b909217f79869aa539c460f7050
곽윤호 <kwakdm95@gmail.com>
2024-01-22 17:37:13 +0630

    change: change pofileImg refference

--- client/src/models/about/about.ts
+++ client/src/models/about/about.ts
@@ -1,33 +1,34 @@
 export const about = {
   profile: {
-    profileIMG: 'https://s.gravatar.com/avatar/23c28fd74ef3561338a391f7492d40a4?s=400',
-    name: 'YounHo Kwak',
-    subName: 'VedK'
+    profileIMG:
+      "https://www.gravatar.com/avatar/e3d305ca3dd159da2aeb67c8994cea36?s=400",
+    name: "YounHo Kwak",
+    subName: "VedK",
   },
   description: [
     {
-      id: 'job',
-      icon: 'taunt',
-      text: 'Developer',
-      href: ''
+      id: "job",
+      icon: "taunt",
+      text: "Developer",
+      href: "",
     },
     {
-      id: 'location',
-      icon: 'location_on',
-      text: 'Yangon Myanmar / Daejeon Korea',
-      href: ''
+      id: "location",
+      icon: "location_on",
+      text: "Yangon Myanmar / Daejeon Korea",
+      href: "",
     },
     {
-      id: 'github',
-      icon: 'code',
-      text: 'github.com/KwakYounHo',
-      href: 'https://github.com/KwakYounHo'
+      id: "github",
+      icon: "code",
+      text: "github.com/KwakYounHo",
+      href: "https://github.com/KwakYounHo",
     },
     {
-      id: 'naverBlog',
-      icon: 'Mode_Comment',
-      text: 'VedK - Naver Blog',
-      href: 'https://blog.naver.com/kwakdm_'
-    }
-  ]
-}
+      id: "naverBlog",
+      icon: "Mode_Comment",
+      text: "VedK - Naver Blog",
+      href: "https://blog.naver.com/kwakdm_",
+    },
+  ],
+};
